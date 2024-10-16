090a631010873d04a1185a1b1cb598faa028f727
jperater123 <jasperperater@gmail.com>
2024-10-16 15:54:15 +0800

    final bug fix

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -30,7 +30,7 @@ const HeroSec = () => {
           </div>
 
           <div className='csv-btn'>
-            <a href='https://portfolio-dusky-nine-72.vercel.app/Jperater-Resume.pdf' download={"Jperater-Resume.pdf"} >DOWNLOAD CV</a> <i className="fa-regular fa-circle-down"></i>
+            <a href='https://portfolio-dusky-nine-72.vercel.app/resume.pdf' download={"resume.pdf"} >DOWNLOAD CV</a> <i className="fa-regular fa-circle-down"></i>
           </div>
         </div>
        
